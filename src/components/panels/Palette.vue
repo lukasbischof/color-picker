b189a5f35b9c05bef9cc43c0816d06faeeb2343f
Lukas Bischof <polinderis@gmail.com>
2018-12-19 23:37:53 +0100

    Align palette

--- src/components/panels/Palette.vue
+++ src/components/panels/Palette.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="palette-container">
-    <div class="item" style="background-color: rgb(255, 0, 0); border: 4px solid black; animation: color-picker-blink-dark 1.5s ease infinite;"
-      title="red"></div>
+    <div class="item active" style="background-color: rgb(255, 0, 0);" title="red"></div>
     <div class="item" style="background-color: rgb(0, 255, 0);" title="green"></div>
     <div class="item" style="background-color: rgb(0, 0, 255);" title="blue"></div>
     <div class="item" style="background-color: rgb(234, 234, 238);" title="aqua"></div>
@@ -22,20 +21,29 @@
 </template>
 
 <style lang="scss" scoped>
+  $tile-size: 70px;
+
   .palette-container {
-    display: flex;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
     flex-wrap: wrap;
+    grid-row-gap: 10px;
+    justify-items: center;
+    padding-top: 5px;
   }
 
   .item {
-    width: 70px;
-    height: 70px;
+    width: $tile-size;
+    height: $tile-size;
     box-sizing: border-box;
+
+    &.active {
+      border: 4px solid black;
+      animation: color-picker-blink-dark 1.5s ease infinite;
+    }
   }
 </style>
 
 <script>
-  export default {
-
-  };
+  export default {};
 </script>
