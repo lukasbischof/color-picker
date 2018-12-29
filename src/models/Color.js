f1983545cb07ff01d762dd04dc1d1910ffc975fc
Lukas Bischof <polinderis@gmail.com>
2018-12-29 18:14:06 +0100

    Refactor color class

--- src/models/Color.js
+++ src/models/Color.js
@@ -1,32 +1,25 @@
 export default class Color {
   static colorFromHex(hex) {
-    const r = 0xFF0000 & hex;
-    const g = 0x00FF00 & hex;
-    const b = 0x0000FF & hex;
+    const mask = 0xff;
+    const r = (mask << 16) & hex;
+    const g = (mask << 8) & hex;
+    const b = mask & hex;
 
-    return new Color(r >> 16, g >> 8, b, 1.0);
+    return new Color(r >> 16, g >> 8, b);
   }
 
-  constructor(r, g, b, a) {
+  constructor(r, g, b, a = 1) {
     this.r = r;
     this.g = g;
     this.b = b;
     this.a = a;
 
-    this.hex = 0x000000;
-    this.hex |= r << 16;
-    this.hex |= g << 8;
-    this.hex = this.hex | b;
-
-    this.cssRGBA = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
-
-    const rStr = r.toString(16).length == 1 ? r.toString(16) + '0' : r.toString(16);
-    const gStr = g.toString(16).length == 1 ? g.toString(16) + '0' : g.toString(16);
-    const bStr = b.toString(16).length == 1 ? b.toString(16) + '0' : b.toString(16);
-    this.cssHex = '#' + rStr + gStr + bStr;
+    this.hex = (r << 16) | (g << 8) | b;
+    this.cssRGBA = `rgba(${[r, g, b, a].join(',')})`;
+    this.cssHex = '#' + [r, g, b].map(n => n.toString().padStart(2, '0')).join();
   }
 
   toString() {
-    return 'Color: r: ' + this.r + ', g: ' + this.g + ', b: ' + this.b + ', a: ' + this.a + ' (Hex: ' + this.cssHex + ')';
+    return `[Color: ${this.cssRGBA}, (${this.cssHex})]`;
   }
 };
